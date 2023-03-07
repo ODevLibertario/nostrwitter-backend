869463df3d9947e87da78dec165064642997ca94
ODevLibertario <odevlibertario@proton.me>
2023-03-07 20:54:29 -0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,10 @@ const port = Number(process.env.PORT) || 3333;
 app.use(bodyParser.text());
 
 app.get('/twitter/auth', (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://nostrwitter.onrender.com'])
     const client = new TwitterApi({ appKey: process.env.APP_KEY, appSecret: process.env.APP_SECRET });
     client.generateAuthLink('oob', { linkMode: 'authorize' }).then(r => {
-        res.setHeader('Access-Control-Allow-Origin', 'https://nostrwitter.onrender.com')
         res.send(r);
     }, error => {
         console.log(error)
@@ -32,7 +33,7 @@ app.post('/twitter/tweet', (req: any, res) => {
 
     const client = new TwitterApi(tokens);
 
-    res.setHeader('Access-Control-Allow-Origin','https://nostrwitter.onrender.com')
+    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://nostrwitter.onrender.com'])
     client.login(pin).then(r => {
             console.log(
                 r.client.v1.tweet(post)
