48b9fde367070f133da26621935e253180853543
odevlibertario <odevlibertario.proton.me>
2023-03-19 22:12:13 -0300

    image upload

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {TwitterApi, TwitterApiTokens} from "twitter-api-v2";
 import 'websocket-polyfill'
 import bodyParser from "body-parser";
+import {ImgurClient} from "imgur";
 require('dotenv').config()
 
 const app = express();
@@ -45,6 +46,27 @@ app.post('/twitter/tweet', (req: any, res) => {
     }, error => res.send(error))
 });
 
+app.get('/ping', (req: any, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://nostrwitter.onrender.com')
+    res.send('pong!')
+})
+
+app.post('/upload', (req: any, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://nostrwitter.onrender.com')
+    const imgurClient = new ImgurClient({ clientId: 'bfd3861f722121a'/*process.env.IMGUR_CLIENT_ID*/ });
+
+    const {imageBase64} = JSON.parse(req.body);
+
+    console.log(typeof imageBase64)
+
+    imgurClient.upload({image: imageBase64, type: "base64"})
+        .then((r) => res.send(r.data.link))
+        .catch(e => {
+                console.log(e)
+                res.send("error")
+            }
+        )
+})
 
 app.listen(port, '0.0.0.0',0,() => {
      console.log(`server is listening on ${port}`);
